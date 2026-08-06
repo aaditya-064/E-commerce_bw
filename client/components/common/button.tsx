cbe9c97fa0824bdf208ec00f642e47a332a1172f
Aaditya Shrestha <aadityastha123456@gmail.com>
2026-08-06 16:01:14 +0545

    add: regiser function and pointer

--- client/components/common/button.tsx
+++ client/components/common/button.tsx
@@ -8,7 +8,7 @@ interface IProps {
 const Button = ({ label, type = "button" }: IProps) => {
   return (
     <button
-      className="rounded-sm w-full py-3 bg-primary text-text-on-primary font-bold text-[18px] hover:bg-primary-hover active:bg-primary-active transition-all duration-300"
+      className="rounded-sm w-full py-3 bg-primary text-text-on-primary font-bold text-[18px] hover:bg-primary-hover active:bg-primary-active transition-all duration-300 cursor-pointer"
       type={type}
     >
       {label}
--- client/components/common/input.tsx
+++ client/components/common/input.tsx
@@ -1,36 +1,33 @@
 import React, { FC } from "react";
+import { UseFormRegister } from "react-hook-form";
 
 interface IProps {
   label: string;
   name: string;
   id: string;
-  type: "text" | "password" | "email";
+  type: "text" | "password" | "email" | "phone";
   placeholder: string;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>) => void;
-  value: string;
+  register: UseFormRegister<any>;
 }
 const Input: FC<IProps> = ({
   id,
   label,
   name,
   placeholder,
-  onChange,
   type = "text",
-  value,
+  register,
 }) => {
   return (
     <div className="flex flex-col gap-2">
-      <label htmlFor={id} className="text-[18px] font-normal tracking-wide">
+      <label htmlFor={id} className="text-[14px] font-normal tracking-wide">
         {label}
       </label>
       <input
         type={type}
         id={id}
-        name={name}
-        onChange={onChange}
         placeholder={placeholder}
-        value={value}
         className="py-2.5 border border-primary px-2 rounded-sm focus:outline-primary text-[18px] placeholder:text-[18px]"
+        {...register(name)}
       />
     </div>
   );
