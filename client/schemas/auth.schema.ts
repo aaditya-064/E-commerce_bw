b469770514436e9484f50c57c99ae44c02ad6b60
Aaditya Shrestha <aadityastha123456@gmail.com>
2026-08-07 15:38:26 +0545

    add: register schema

--- client/schemas/auth.schema.ts
+++ client/schemas/auth.schema.ts
@@ -5,3 +5,11 @@ export const loginSchema = yup.object({
   email: yup.string().email().required("email is required"),
   password: yup.string().required("password is required"),
 });
+
+export const registerSchema = yup.object({
+  full_name: yup.string().required("full_name is required"),
+  email: yup.string().email().required("email is required"),
+  password: yup.string().required("password is required"),
+  phone: yup.string(),
+  c_password: yup.string(),
+});
