5a666bb87d2e4f237ffca64e57e5eb7e7a26e221
Aaditya Shrestha <aadityastha123456@gmail.com>
2026-08-08 15:54:34 +0545

    add: drop cart function

--- server/src/routes/cart.routes.ts
+++ server/src/routes/cart.routes.ts
@@ -1,5 +1,11 @@
 import express from "express";
-import { addQuantity, create, get } from "../controller/cart.controller";
+import {
+  addQuantity,
+  create,
+  drop,
+  get,
+  removeProduct,
+} from "../controller/cart.controller";
 import { authenticate } from "../middlewares/auth.middleware";
 import { All_Admins } from "../types/enum.types";
 const router = express.Router();
@@ -12,7 +18,12 @@ const router = express.Router();
 router.get("/:id", authenticate(All_Admins), get);
 router.post("/", authenticate(All_Admins), create);
 router.post("/add", authenticate(All_Admins), addQuantity);
+router.delete(
+  "/remove/:userId/:productId",
+  authenticate(All_Admins),
+  removeProduct,
+);
+router.delete("/delete", authenticate(All_Admins), drop);
 // router.patch("/update", updateBrand);
-// router.delete("/delete", deleteBrand);
 
 export default router;
