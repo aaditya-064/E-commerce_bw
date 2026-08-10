3542297d95bca524c7c1dfddcc91995bb887ae9d
Aaditya Shrestha <aadityastha123456@gmail.com>
2026-08-10 15:32:12 +0545

    add: nav and footer

--- client/api/product.api.ts
+++ client/api/product.api.ts
@@ -1,1 +1,47 @@
+import { TProduct } from "@/types/product.types";
 import { api } from ".";
+
+export const create = async (data: TProduct) => {
+  try {
+    const response = await api.post("/product", data);
+    return response.data;
+  } catch (error: any) {
+    throw error.response.data;
+  }
+};
+
+export const get = async () => {
+  try {
+    const response = await api.get("/product");
+    return response.data;
+  } catch (error: any) {
+    throw error.response.data;
+  }
+};
+
+export const getById = async (id: string) => {
+  try {
+    const response = await api.get(`/${id}`);
+    return response.data;
+  } catch (error: any) {
+    throw error.response.data;
+  }
+};
+
+// export const update = async (data: TProduct, id: string) => {
+//   try {
+//     const response = await api.put(`/${id}`, data);
+//     return response.data;
+//   } catch (error: any) {
+//     throw error.response.data;
+//   }
+// };
+
+// export const remove = async (id: string) => {
+//   try {
+//     const response = await api.delete(`/${id}`);
+//     return response.data;
+//   } catch (error: any) {
+//     throw error.response.data;
+//   }
+// };
